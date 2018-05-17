af781bd37c793567f1b8a49747726d47daa3cc8c
Gregor <gregor.gorjanc@student.um.si>
2018-05-17 11:46:35 +0200

    d

--- src/ejb/OsebaVmesnik.java
+++ src/ejb/OsebaVmesnik.java
@@ -1,11 +1,16 @@
 package ejb;
 
+import vao.Oseba;
+
 import javax.ejb.Local;
+import java.util.List;
 
 @Local
 public interface OsebaVmesnik {
 
-
+    void dodajOsebo(Oseba o);
+    void odstraniOsebo(Oseba o);
+    List<Oseba> vrniSeznamVsehOseb();
 
 
 }
