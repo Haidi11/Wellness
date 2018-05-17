5c9eb284f01fb4daab79abfe6288d95658161a70
Haidi <zakelsek.haidi@gmail.com>
2018-05-17 11:03:46 +0100

    Priprava entitete Oseba in pogleda za prijavo.

---
+++ src/jsf/OsebaJPA.java
@@ -0,0 +1,46 @@
+package org.primefaces.showcase.view.input;
+
+import javax.faces.bean.ManagedBean;
+
+@Entity(name="Oseba")
+public class Oseba {
+
+    private int id;
+    private String ime;
+    private String priimek;
+    private String email;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPriimek() {
+        return priimek;
+    }
+
+    public void setPriimek(String priimek) {
+        this.priimek = priimek;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
