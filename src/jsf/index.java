f6b72d131ca29fe5e71c35b4d712b0c985dccf1c
Gregor <gregor.gorjanc@student.um.si>
2018-05-17 10:52:12 +0200

    d

--- src/jsf/index.java
+++ src/jsf/index.java
@@ -1,4 +1,14 @@
 package jsf;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="index")
+@SessionScoped
 public class index {
+
+
+    private String nekaj="keks";
+
+
 }
