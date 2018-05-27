331690149fd51e5776aaf0dd55accb92295523d5
Gregor Gorjanc <gregor.gorjanc18@gmail.com>
2018-05-27 15:45:22 +0200

    prikaz uporabnika

--- src/jsf/ZaposleniModel.java
+++ src/jsf/ZaposleniModel.java
@@ -11,6 +11,7 @@ import javax.faces.context.FacesContext;
 import ejb.DogodekVmesnik;
 import orodja.PaketZaprikazDogodkov;
 import vao.Dogodek;
+import vao.Oseba;
 
 @ManagedBean(name = "zaposleni")
 @SessionScoped
@@ -18,11 +19,11 @@ public class ZaposleniModel {
 
 	@EJB
 	DogodekVmesnik dv;
-	
-	//za prikaz stilov glede na prijavljenost
+
+	// za prikaz stilov glede na prijavljenost
 	private String prijave;
-	
-	//pridobi uporabnika
+
+	// pridobi uporabnika
 	private Principal vrniUporabnika() {
 		FacesContext fc = FacesContext.getCurrentInstance();
 		return fc.getExternalContext().getUserPrincipal();
@@ -30,19 +31,24 @@ public class ZaposleniModel {
 
 	// uporabnik izbere dogodek, ta se doda na njegov seznam dogodkov
 	public void izberiDogodek(Dogodek d) {
-		Principal p = vrniUporabnika();	
+		Principal p = vrniUporabnika();
 		dv.izberiDogodek(d, p.getName());
 	}
 
 	// prikaz dogodkov z dodanimi lastnostmi o prijavljenosti
 	public List<Dogodek> getVrniVseDogodkeZaUporabnika() {
-		PaketZaprikazDogodkov p=dv.sezamDogodkovZaUporabnika(vrniUporabnika().getName());
-		prijave=p.getSeznamRazredov();
-		//System.out.println(prijave);
+		PaketZaprikazDogodkov p = dv.sezamDogodkovZaUporabnika(vrniUporabnika().getName());
+		prijave = p.getSeznamRazredov();
+		// System.out.println(prijave);
 		return p.getSeznam();
 	}
-
 	
+	//za prikaz imena, vloge
+	public Oseba getUporabnik() {
+		return dv.najdiPoUporabniskemImenu(vrniUporabnika().getName());
+		
+	}
+
 	/*
 	 * getterji, setterji
 	 */
@@ -53,6 +59,5 @@ public class ZaposleniModel {
 	public void setPrijave(String prijave) {
 		this.prijave = prijave;
 	}
-	
-		
+
 }
