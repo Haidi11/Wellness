440e09312284515ded44be1db3540e90d57375f4
Haidi11 <haidi.zakelsek@student.um.si>
2018-05-28 16:06:57 +0100

    shranjevanje blockchaina v pb

--- src/ejb/BlockChainBean.java
+++ src/ejb/BlockChainBean.java
@@ -14,19 +14,17 @@ import vmesniki.BlockChainVmesnik;
 
 @Stateless
 public class BlockChainBean implements BlockChainVmesnik {
-	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 	public static int tezavnost = 0;
-	Block block;
-	
+
 	@PersistenceContext
-	EntityManager em; 
+	EntityManager em;
 
 	@Override
 	public void shrani(Tocke t) {
 		String prejsnjiHash = najdiPrejsnjiHash();
 		System.out.println("tocka: " + t.getId() + ", oseba: " + t.getIdOseba() + ", dogodek: " + t.getIdDogodek());
 		try {
-			blockchain.add(new Block(t.getId(), t.getIdOseba(), t.getIdDogodek(), prejsnjiHash));
+			shraniBlockChain(new Block(t.getId(), t.getIdOseba(), t.getIdDogodek(), prejsnjiHash));
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,41 +32,14 @@ public class BlockChainBean implements BlockChainVmesnik {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for (int i = 0; i == blockchain.size(); i++) {
-			if (i == 0) {
-				block.setPrejsnjiHash("0");
-				try {
-					blockchain.get(i).rudarjenjeBloka(tezavnost);
-				} catch (NoSuchAlgorithmException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (UnsupportedEncodingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				System.out.println("Hash trenutnega bloka: " + blockchain.get(i).getHash() + ", idTocke: "
-						+ blockchain.get(i).getIdTocke());
-			}
-			try {
-				blockchain.get(i).rudarjenjeBloka(tezavnost);
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("Hash trenutnega bloka: " + blockchain.get(i).getHash() + ", idTocke: "
-					+ blockchain.get(i).getIdTocke());
-
-		}
 	}
-	
+
 	public String najdiPrejsnjiHash() {
-		String prejsnjiHash = (String) em.createQuery("select b from Block b order by b.timestamp desc").getResultList().get(0);
+		Block o =  (Block) em.createQuery("select b from Block b order by b.timeStamp desc").getResultList().get(0);
+		String prejsnjiHash = o.getHash();
 		return prejsnjiHash;
 	}
-	
+
 	public void shraniBlockChain(Block block) {
 		em.persist(block);
 	}
