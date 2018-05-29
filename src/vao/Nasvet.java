853ab670cf0e27bc8130fc6c6b5f1b28d836a800
Haidi11 <haidi.zakelsek@student.um.si>
2018-05-29 08:31:31 +0100

    Popravek v bc in entiteta nasvet

---
+++ src/vao/Nasvet.java
@@ -0,0 +1,32 @@
+package vao;
+
+public class Nasvet {
+	private int idNasvet;
+	private String nasvet;
+	private String avtor;
+	private long timeStamp;
+	public int getIdNasvet() {
+		return idNasvet;
+	}
+	public void setIdNasvet(int idNasvet) {
+		this.idNasvet = idNasvet;
+	}
+	public String getNasvet() {
+		return nasvet;
+	}
+	public void setNasvet(String nasvet) {
+		this.nasvet = nasvet;
+	}
+	public String getAvtor() {
+		return avtor;
+	}
+	public void setAvtor(String avtor) {
+		this.avtor = avtor;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}	
+}
