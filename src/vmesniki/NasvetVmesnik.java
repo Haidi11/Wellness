ff34dac3d9bc0f0973bbbb3a4bfd58e33b3e856f
Haidi11 <haidi.zakelsek@student.um.si>
2018-05-29 21:54:10 +0100

    Manjsi popravki za custom prikaz nasvetov

--- src/vmesniki/NasvetVmesnik.java
+++ src/vmesniki/NasvetVmesnik.java
@@ -1,6 +1,5 @@
 package vmesniki;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +12,20 @@ import vao.Oseba;
 @Local
 public interface NasvetVmesnik {
 	void dodajNasvet(Nasvet nasvet);
+
 	void brisiNasvet(Nasvet nasvet);
+
 	void urediNasvet(Nasvet nasvet);
+
 	ArrayList<Nasvet> seznamVsehNasvetov();
+
 	PaketZaPrikazNasvetov sezamDogodkovZaUporabnika(int id);
+
 	Nasvet najdiPoIdAvtorja(int id);
+
 	List<Nasvet> getNasveteAvtorja(int id);
+
+	int najdiIdAvtorja(String ime);
+
+	List<Nasvet> getVseNasveteAvtorja();
 }
