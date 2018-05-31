8a591a812847cde18b0bd93fe21509029dee6fd3
Gregor <gregor.gorjanc18@gmail.com>
2018-05-31 16:59:10 +0200

    Ure ne delajo ker so ***

--- src/orodja/Oro.java
+++ src/orodja/Oro.java
@@ -9,7 +9,26 @@ public class Oro {
 		if (c == null)
 			return "";
 		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+		return format.format(c.getTime()) + " " +ura(c);
+		
+	}
+	public static String ura(Calendar c) {
+//		String ura ="";
+//		String min="";
+//		
+//		ura += c.get(Calendar.HOUR_OF_DAY);
+//		min += c.get(Calendar.MINUTE);
+//		
+//		if (ura.length()==1)
+//			ura="0"+ura;
+//		if(min.length()==1)
+//			min="0"+min;
+//		
+//		return ura + ":"+ min;
+		
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm");
 		return format.format(c.getTime());
+				
 		
 	}
 }
