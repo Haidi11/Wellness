189d0d3cd488a13f27576dfcbcdaa4bca05e70d3
Gregor <gregor.gorjanc18@gmail.com>
2018-06-06 20:49:50 +0200

    zbrisi nasvet iz baze

--- src/vao/Nasvet.java
+++ src/vao/Nasvet.java
@@ -6,11 +6,13 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -24,6 +26,7 @@ public class Nasvet {
 	@ManyToOne
     @JoinColumn(name = "fk_avtor")
 	private Oseba avtor;
+	@Column(length=10000)
 	private String nasvet;
 	private long timeStamp;
 	
